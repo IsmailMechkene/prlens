5f7e3db5714b616fb0d282465365593714584c7f
Ismail Mechkene <imechkene@gmail.com>
2026-06-04 11:31:54 +0100

    Add GitHubClient with auth and repo access(PAT)

---
+++ prlens/github/client.py
@@ -0,0 +1,24 @@
+from github import Github, Repository, GithubException
+from dotenv import load_dotenv
+import os
+
+class GitHubClient:
+    def __init__(self):
+
+        load_dotenv()
+        self.token = os.getenv("GITHUB_TOKEN")
+
+        if not self.token:
+            raise ValueError("GITHUB_TOKEN not found in environment")
+
+        try:
+            self.client = Github(self.token)
+        except Exception as e:
+            print(f"An error occurred when connecting to GitHub: {e}")
+
+    def get_repo(self, repo_name: str) -> Repository.Repository: #Return type ->
+        try:
+            repo = self.client.get_repo(repo_name)
+        except GithubException as ge:
+            raise ValueError(f"Could not access repository '{repo_name}': {ge}")
+        return  repo
