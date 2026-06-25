c81ba5f8dcbee5a8372fcbcac8ddc38e28c52fe9
Ismail Mechkene <imechkene@gmail.com>
2026-06-25 15:24:02 +0100

    Add post_inline_comments method to PRPublisher

--- prlens/github/pr_publisher.py
+++ prlens/github/pr_publisher.py
@@ -1,14 +1,36 @@
 from prlens.config.settings import Settings
 from prlens.github.client import GitHubClient
 from github.PullRequest import PullRequest
-from prlens.models.review import ReviewType, ReviewResult
+from prlens.models.review import ReviewType, ReviewResult, Severity, ReviewComment
 
 
 class PRPublisher:
     def __init__(self, client: GitHubClient):
         self.client = client
 
-    def post_inline_comments(self, pull_request, comments):
+    def post_inline_comments(self, pull_request: PullRequest, comments: list[ReviewComment]) -> None:
+        SEVERITY_EMOJI = {
+            Severity.INFO: "🔵",
+            Severity.WARNING: "🟡",
+            Severity.ERROR: "🟠",
+            Severity.CRITICAL: "🔴",
+        }
+
+        commit = pull_request.get_commits().reversed[0]
+        for comment in comments:
+            emoji = SEVERITY_EMOJI.get(comment.severity)
+            body = f"{emoji} {comment.message}"
+
+            if comment.suggestion:
+                body += f"\n\n💡 **Suggestion:** {comment.suggestion}"
+
+            if not comment.line:
+                pull_request.create_issue_comment(body=body)
+            else:
+                pull_request.create_review_comment(body=body, commit=commit, path=comment.file_path,
+                                                   line=comment.line)
+
+
 
     def post_summary(self, pull_request, result):
 
@@ -31,6 +53,8 @@ class PRPublisher:
             labels.append("documentation-needed")
 
         pull_request.set_labels(*labels)
+
+
 
     def submit_review(self, pull_request: PullRequest, result: ReviewResult) -> None:
         if result.score > 80 and not result.has_critical_issues:
@@ -63,6 +87,8 @@ class PRPublisher:
                 ),
                 event="COMMENT"
             )
+
+
 
     def assign_reviewers(self, pull_request: PullRequest, result: ReviewResult, settings: Settings) -> None:
         if not result.has_critical_issues:
